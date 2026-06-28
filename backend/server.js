48ca19671fd35b456bbf032e9567be9bf2d4fda9
vivek <viveksr012@gmail.com>
2026-06-28 16:54:59 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,12 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-  }),
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL || "http://localhost:5173",
+//   }),
+// );
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
